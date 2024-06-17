072c06cd2cb3435e1e8a2163ca43c15140aafb42
Alexander Sanchez G <alexander_sg29@outlook.com>
2024-06-16 20:29:28 -0500

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,9 @@ module.exports = {
       'Bright-Orange': 'hsl(31, 77%, 52%)',
       'Dark-Cyan': 'hsl(184, 100%, 22%)',
       'Very-Dark-Cyan': 'hsl(179, 100%, 13%)',
-      'Transparent-White': 'hsla(0, 0%, 100%, 0.75)',
-      'Very-Light-Gray': 'hsl(0, 0%, 95%)'
+      'Transparent-White': 'hsl(0, 0%, 100%, 0.75)',
+      'Very-Light-Gray': 'hsl(0, 0%, 95%)',
+      'white': 'hsl(0, 0%, 100%)',
     },
     fontFamily: {
       sans: ['Lexend Deca', 'sans-serif'],
